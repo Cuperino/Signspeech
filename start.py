36713b6b9110716d5657f581a8db2964d234c221
javiercordero <javier@imaginary.tech>
2019-11-03 04:50:00 -0400

    "What is your name?" translates successfully.

--- start.py
+++ start.py
@@ -40,6 +40,7 @@ import azure.cognitiveservices.speech as speechsdk
 import os
 import subprocess
 import stanfordnlp
+from operator import itemgetter, attrgetter, methodcaller
 
 # Download models on first run
 # stanfordnlp.download('en')   # This downloads the English models for the neural pipeline
@@ -91,42 +92,202 @@ def getSpeech():
 def parse(text):
   # Process text input
   doc = nlp(text) # Run the pipeline on text input
-  print("\n")
 
-  # Look at the results
+  print ("""
+  ┌─┐┌─┐┬─┐┌─┐┌─┐┬─┐┌┬┐  ┌┬┐┬─┐┌─┐┌┐┌┌─┐┬  ┌─┐┌┬┐┬┌─┐┌┐┌
+  ├─┘├┤ ├┬┘├┤ │ │├┬┘│││   │ ├┬┘├─┤│││└─┐│  ├─┤ │ ││ ││││
+  ┴  └─┘┴└─└  └─┘┴└─┴ ┴   ┴ ┴└─┴ ┴┘└┘└─┘┴─┘┴ ┴ ┴ ┴└─┘┘└┘
+  """)
+
   for sentence in doc.sentences:
-    for word in sentence.words:
-      print(word)
-  # doc.sentences[0].print_tokens() # Look at the result
-  # doc.sentences[0].print_dependencies()
   
-  return doc.sentences
+    translation = translate(sentence)
 
-def reorder(parse):
-  aslTree = parse
+    result = []
+    for word in translation[0]:
+      result.append((word.text.lower(), word.lemma.lower()))
+    print("\nResult: ", result, "\n")
 
-  return aslTree
+    print ("""
+  ┌─┐┌─┐┬    ┬─┐┌─┐┌─┐┬─┐┌─┐┌─┐┌─┐┌┐┌┌┬┐┌─┐┌┬┐┬┌─┐┌┐┌
+  ├─┤└─┐│    ├┬┘├┤ ├─┘├┬┘├┤ └─┐├┤ │││ │ ├─┤ │ ││ ││││
+  ┴ ┴└─┘┴─┘  ┴└─└─┘┴  ┴└─└─┘└─┘└─┘┘└┘ ┴ ┴ ┴ ┴ ┴└─┘┘└┘
+    """)
+    display(translation)
 
-def reduce(tree):
-  return tree
+  return doc
 
-def getLemmasOnly(tree):
+def wordToDictionary(word):
+  dictionary = {
+    'index': word.index,
+    'governor': word.governor,
+    'text': word.text.lower(),
+    'lemma': word.lemma.lower(),
+    'upos': word.upos,
+    'xpos': word.xpos,
+    'dependency_relation': word.dependency_relation,
+    'feats': word.dependency_relation,
+    'children': []
+  }
+  return dictionary
+
+
+def getMeta(sentence):
+  # sentence.print_dependencies()
+  englishStruct = {}
+  aslStruct = {
+    'rootElements':[],
+    'UPOS': {
+      'ADJ':[], 'ADP':[], 'ADV':[], 'AUX':[], 'CCONJ':[], 'DET':[], 'INTJ':[], 'NOUN':[], 'NUM':[], 'PART':[], 'PRON':[], 'PROPN':[], 'PUNCT':[], 'SCONJ':[], 'SYM':[], 'VERB':[], 'X':[]
+    }
+  }
+  reordered = []
+  # aslStruct["rootElements"] = []
+
+  # Make a list of all tokenized words. This step might be unnecessary.
+  words = []
+  for token in sentence.tokens:
+    # print(token)
+    for word in token.words:
+      
+      print(word.index, word.governor, word.text, word.lemma, word.upos, word.dependency_relation) # , word.feats)
+      # # Insert as dict
+      # words.append(wordToDictionary(word))
+      # Insertion sort
+      j = len(words)
+      for i, w in enumerate(words):
+        if word.governor <= w.governor:
+          continue
+        else:
+          j = i
+          break
+      words.insert(j, word)
+  # # Python sort for converted words
+  # words.sort(key=attrgetter('governor', 'age')) # , reverse=True
+  # words.sort(key=words.__getitem__) # , reverse=True
+  reordered = words
+
+  # Deprecated aslStruct code...
+  # While there exist words that haven't been added to the tree.  
+  # englishStruct['root'] = wordToDictionary(words[0])
+  #     # Create list of words for each UPOS
+  #     aslStruct['UPOS'][word.upos].append(word)
+  # 
+  # # Sort each UPOS list
+  # # print(aslStruct['UPOS'])
+  # for upos, uposList in aslStruct['UPOS'].items():
+  #   # print(upos, uposList)
+  #   uposList.sort(key=attrgetter('governor'))
+  #   print(upos, uposList)
+
+  # Identify Root Elements
+  # for word in token.words:
+    # if word.dependency_relation == "root":
+      # aslStruct["rootElements"].append(word)
+      # Get related elements
+      # Ident topics & comments
+
+  # print("\n", aslStruct, "\n")
+  return reordered
+
+def getLemmaSequence(meta):
+  tone = ""
   translation = []
+  for word in meta:
+    # Remove blacklisted words
+    if (word.text.lower(), word.lemma.lower()) not in (('is', 'be'), ('the', 'the'), ('of', 'the'), ('is', 'are'), ('by', 'by'), (',', ','), (';', ';'), (':'), (':')):
+      
+      # Get Tone: get the sentence's tone from the punctuation
+      if word.upos == 'PUNCT':
+        if word.lemma == "?":
+          tone = "?"
+        elif word.lemma == "!":
+          tone = "!"
+        else:
+          tone = ""
+        continue
+      
+      # Remove symbols and the unknown
+      elif word.upos == 'SYM' or word.upos == 'X':
+        continue
+      
+      # Remove particles
+      if word.upos == 'PART':
+        continue
+
+      # Convert proper nouns to finger spell
+      elif word.upos == 'PROPN':
+        fingerSpell = []
+        for letter in word.text.lower():
+          spell = {}
+          spell['text'] = letter
+          spell['lemma'] = letter
+          # Add fingerspell as individual lemmas
+          fingerSpell.append(spell)
+
+      # Numbers
+      elif word.upos == 'NUM':
+        fingerSpell = []
+        for letter in word.text.lower():
+          spell = {}
+          # Convert number to fingerspell
+          pass
+          # Add fingerspell as individual lemmas
+          fingerSpell.append(spell)
+
+      # Interjections usually use alternative or special set of signs
+      elif word.upos == 'CCONJ':
+        translation.append(word)
+      
+      # Interjections usually use alternative or special set of signs
+      elif word.upos == 'SCONJ':
+        if (word.text.lower(), word.lemma.lower() not in (('that', 'that'))):
+          translation.append(word)
+      
+      # Interjections usually use alternative or special set of signs
+      elif word.upos == 'INTJ':
+        translation.append(word)
+
+      # Adpositions could modify nouns
+      elif word.upos=='ADP':
+        translation.append(word)
+        # pass
+
+      # Determinants modify noun intensity
+      elif word.upos=='DET':
+        pass
+
+      # Adjectives modify nouns and verbs
+      elif word.upos=='ADJ':
+        translation.append(word)
+        # pass
+
+      # Nouns
+      elif word.upos == 'NOUN' or word.upos == 'PRON':
+        translation.append(word)
+
+      # Adverbs modify verbs
+      elif word.upos=='ADV' or word.upos=='AUX':
+        translation.append(word)
+
+      # Verbs
+      elif word.upos=='VERB' or word.upos=='':
+        translation.append(word)
+
   # translation = tree
+  return (translation, tone)
+
+def translate(parse):
+  meta = getMeta(parse)
+  translation = getLemmaSequence(meta)
   return translation
 
-def translate(text, parse):
-  aslTree = reorder(parse)
-  reducedAslTree = reduce(aslTree)
-  translation = getLemmasOnly(reducedAslTree)
-  return translation
-
-def display(lemmas):
+def display(translation):
   folder = os.getcwd()
   filePrefix = folder + "/videos/"
   # Alter ASL lemmas to match sign's file names.
   # In production, these paths would be stored at the dictionary's database.
-  files = [ filePrefix + lemma + "_.mp4" for lemma in lemmas ]
+  files = [ filePrefix + word.text.lower() + "_.mp4" for word in translation[0] ]
   # Run video sequence using the MLT Multimedia Framework
   print("Running command: ", ["melt"] + files)
   process = subprocess.Popen(["melt"] + files + [filePrefix + "black.mp4"], stdout=subprocess.PIPE)
@@ -143,40 +304,24 @@ def main():
   # tests = []
   tests = [
     "What is your name?",
-    "Where is the bathroom?",
-    "Love computers!"
+    # "My name is Javier.",
+    # "Bring your computer!",
+    # "Where is the bathroom?",
+    # "Ana was on her way to school when she suddenly remembered!"
   ]
 
   if len(tests) == 0:
     tests = tests + [ getSpeech() ]
 
   for text in tests:
-    print("\nText to process: ", text, "\n")
-
     print ("""
-    ┌─┐┌┐┌┌─┐┬ ┬ ┬┌─┐┌─┐  ┌─┐┌┐┌┌─┐┬  ┬┌─┐┬ ┬
-    ├─┤│││├─┤│ └┬┘└─┐├┤   ├┤ ││││ ┬│  │└─┐├─┤
-    ┴ ┴┘└┘┴ ┴┴─┘┴ └─┘└─┘  └─┘┘└┘└─┘┴─┘┴└─┘┴ ┴
+  ┌─┐┌┐┌┌─┐┬ ┬ ┬┌─┐┌─┐  ┌─┐┌┐┌┌─┐┬  ┬┌─┐┬ ┬
+  ├─┤│││├─┤│ └┬┘└─┐├┤   ├┤ ││││ ┬│  │└─┐├─┤
+  ┴ ┴┘└┘┴ ┴┴─┘┴ └─┘└─┘  └─┘┘└┘└─┘┴─┘┴└─┘┴ ┴
     """)
 
-    parsed = parse(text)
-    print("Parse", parsed)
+    print("Text to process: ", text, "\n")
 
-    print ("""
-    ┌─┐┌─┐┬─┐┌─┐┌─┐┬─┐┌┬┐  ┌┬┐┬─┐┌─┐┌┐┌┌─┐┬  ┌─┐┌┬┐┬┌─┐┌┐┌
-    ├─┘├┤ ├┬┘├┤ │ │├┬┘│││   │ ├┬┘├─┤│││└─┐│  ├─┤ │ ││ ││││
-    ┴  └─┘┴└─└  └─┘┴└─┴ ┴   ┴ ┴└─┴ ┴┘└┘└─┘┴─┘┴ ┴ ┴ ┴└─┘┘└┘
-    """)
-    
-    translation = translate(text, parse)
-    translation = ["your", "name", "what"]
-    print("\nResult: ", translation, "\n")
-
-    # print ("""
-    # ┌─┐┌─┐┬    ┬─┐┌─┐┌─┐┬─┐┌─┐┌─┐┌─┐┌┐┌┌┬┐┌─┐┌┬┐┬┌─┐┌┐┌
-    # ├─┤└─┐│    ├┬┘├┤ ├─┘├┬┘├┤ └─┐├┤ │││ │ ├─┤ │ ││ ││││
-    # ┴ ┴└─┘┴─┘  ┴└─└─┘┴  ┴└─└─┘└─┘└─┘┘└┘ ┴ ┴ ┴ ┴ ┴└─┘┘└┘
-    # """)
-    # display(translation)
+    parse(text)
 
 main()
